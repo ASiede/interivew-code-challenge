39e8f0f0f58245d3fc7c7100e226783b60e96d73
Andrea Siede <andrea.siede@gmail.com>
2019-01-23 09:20:15 -0800

    added ex1 tests

--- exercise-one/index.test.js
+++ exercise-one/index.test.js
@@ -2,12 +2,34 @@ const { invertedArrayMode } = require('./')
 
 describe('invertedArrayMode', () => {
   it('should invert the list and append .js when in prod mode', () => {
-    expect(invertedArrayMode())
-      .toBe(undefined)
+  	const testCase = {
+	  input: 'prod',
+	  expected: [
+	    'grass.js',
+		'blue.js',
+		'sky.js',
+		'green.js',
+		'mountain.js',
+		'red.js'
+		]
+  	}
+    expect(invertedArrayMode(testCase.input))
+      .toBe(testCase.expected)
   })
 
   it('should invert the list and append .test.js in dev mode', () => {
+  	const testCase = {
+	  input: 'dev',
+	  expected: [
+	    'grass.test.js',
+		'blue.test.js',
+		'sky.test.js',
+		'green.test.js',
+		'mountain.test.js',
+		'red.test.js'
+		]
+  	}
     expect(invertedArrayMode())
-      .toBe(undefined)
+      .toBe(testCase.expected)
   })
 })
