8d483de207040688f6c9b7a8a21b526a07155a42
Andrea Siede <andrea.siede@gmail.com>
2019-01-23 12:07:19 -0800

    added ex2 functions

--- exercise-two/index.js
+++ exercise-two/index.js
@@ -1,8 +1,34 @@
 const data = require('./data')
 
+const uniqueShortIds = [];
+
+function shortifyId(string) {
+  for(i=2; i<string.length; i++)
+  // ensure unique ID
+  if (!uniqueShortIds.includes(string[0].concat(string[1], string[i]))) {
+    uniqueShortIds.push(string[0].concat(string[1], string[i]));
+    return string[0].concat(string[1], string[i]);
+  }
+}
+
 const generateShortIds = (id) => {
-  // TODO: write this function
-  return undefined
+  for (i=0; i<data.categories.length; i++) {
+    if(data.categories[i]["id"] === id) {
+      data.categories[i]["shortId"] = shortifyId(id);
+      return data.categories;
+    } else {
+      for(j=0; j<data.categories[i]["items"].length; j++) {
+        if (data.categories[i]["items"][j]["id"] === id) {
+          data.categories[i]["items"][j]["shortId"] = shortifyId(id);
+          return data.categories;
+        } else if (data.categories[i]["items"][j]["skuData"] && data.categories[i]["items"][j]["skuData"]["id"] === id) {
+          data.categories[i]["items"][j]["skuData"]["shortId"] = shortifyId(id);
+          return data.categories;
+        }
+      }
+    }
+  }
+  return data.categories;  
 }
 
 module.exports = {
