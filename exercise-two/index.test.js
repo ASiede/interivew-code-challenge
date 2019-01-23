6c3e7e22c655678b49975887b16aa175ac949559
Andrea Siede <andrea.siede@gmail.com>
2019-01-23 12:01:43 -0800

    added test1 for ex2

--- exercise-two/index.test.js
+++ exercise-two/index.test.js
@@ -2,12 +2,19 @@ const { generateShortIds } = require('./')
 
 describe('generateShortIds', () => {
   it('should create a shortId prop to the given id', () => {
-    expect(generateShortIds())
-      .toBe(undefined)
+  	//Testing for each layer of nesting in first element and second element
+    expect(generateShortIds("a6e5dc32-1c6c-11e9-ab14-d663bd873d93")[0])
+      .toHaveProperty("shortId", 'a6e')
+    expect(generateShortIds("34271080-1c6c-11e9-ab14-d663bd873d93")[0]["items"][0])
+      .toHaveProperty("shortId", '342')
+    expect(generateShortIds("34271332-1c6c-11e9-ab14-d663bd873d93")[0]["items"][0]["skuData"])
+      .toHaveProperty("shortId", '347')
+    expect(generateShortIds("342718a0-1c6c-11e9-ab14-d663bd873d93")[1])
+      .toHaveProperty("shortId", '341')      
   })
 
-  it('should create shortId to all object with an existing id prop', () => {
-    expect(generateShortIds())
-      .toBe(undefined)
-  })
+  // it('should create shortId to all object with an existing id prop', () => {
+  //   expect(generateShortIds())
+  //     .toBe(undefined)
+  // })
 })
