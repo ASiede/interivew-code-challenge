2afb84aa42e8258d2db19e018a80ea747beb868f
Andrea Siede <andrea.siede@gmail.com>
2019-01-24 11:05:47 -0800

    refactored for test data layer

--- exercise-two/index.js
+++ exercise-two/index.js
@@ -1,6 +1,6 @@
 const data = require('./data')
  
-const uniqueShortIds = [];
+const uniqueShortIds = [];  
 
 function shortifyId(string) {
   for(k=2; k<string.length; k++)
@@ -11,40 +11,40 @@ function shortifyId(string) {
   }
 }
 
-const generateShortIds = (id) => {
+const generateShortIds = (data, id) => {
   if (id) {
-  	for (i=0; i<data.categories.length; i++) {
-    	if(data.categories[i]["id"] === id) {
-				data.categories[i]["shortId"] = shortifyId(id);
-				return data.categories;
-			} else {
-				for(j=0; j<data.categories[i]["items"].length; j++) {
-					if (data.categories[i]["items"][j]["id"] === id) {
-						data.categories[i]["items"][j]["shortId"] = shortifyId(id);
-						return data.categories;
-					} else if (data.categories[i]["items"][j]["skuData"] && data.categories[i]["items"][j]["skuData"]["id"] === id) {
-						data.categories[i]["items"][j]["skuData"]["shortId"] = shortifyId(id);
-						return data.categories;
-					}
-				}
-			}
-		}
+    for (i=0; i<data.categories.length; i++) {
+      if(data.categories[i]["id"] === id) {
+	    data.categories[i]["shortId"] = shortifyId(id);
+		return data.categories;
+	  } else {
+	    for(j=0; j<data.categories[i]["items"].length; j++) {
+	      if (data.categories[i]["items"][j]["id"] === id) {
+		   data.categories[i]["items"][j]["shortId"] = shortifyId(id);
+		   return data.categories;
+		  } else if (data.categories[i]["items"][j]["skuData"] && data.categories[i]["items"][j]["skuData"]["id"] === id) {
+		    data.categories[i]["items"][j]["skuData"]["shortId"] = shortifyId(id);
+		    return data.categories;
+		  }
+	    }
+      }
+    }
 
-	} else if (!id) {
-		for (i=0; i<data.categories.length; i++) {
-		    if(data.categories[i]["id"] && !data.categories[i]["shortId"]) {
-		      data.categories[i]["shortId"] = shortifyId(data.categories[i]["id"]);
-		      for(j=0; j<data.categories[i]["items"].length; j++) {
-		        if (data.categories[i]["items"][j]["id"] && !data.categories[i]["items"][j]["shortId"]) {
-		          data.categories[i]["items"][j]["shortId"] = shortifyId(data.categories[i]["items"][j]["id"]);
-		        }
-		        if (data.categories[i]["items"][j]["skuData"] && data.categories[i]["items"][j]["skuData"]["id"] && !data.categories[i]["items"][j]["skuData"]["shortId"]) {
-		          data.categories[i]["items"][j]["skuData"]["shortId"] = shortifyId(data.categories[i]["items"][j]["skuData"]["id"]);  
-		        }
-		      }  		     
-		    }  
-	    } 
-	}
+  } else if (!id) {
+    for (i=0; i<data.categories.length; i++) {
+      if(data.categories[i]["id"] && !data.categories[i]["shortId"]) {
+	    data.categories[i]["shortId"] = shortifyId(data.categories[i]["id"]);
+	    for(j=0; j<data.categories[i]["items"].length; j++) {
+	      if (data.categories[i]["items"][j]["id"] && !data.categories[i]["items"][j]["shortId"]) {
+            data.categories[i]["items"][j]["shortId"] = shortifyId(data.categories[i]["items"][j]["id"]);
+          }
+	      if (data.categories[i]["items"][j]["skuData"] && data.categories[i]["items"][j]["skuData"]["id"] && !data.categories[i]["items"][j]["skuData"]["shortId"]) {
+	        data.categories[i]["items"][j]["skuData"]["shortId"] = shortifyId(data.categories[i]["items"][j]["skuData"]["id"]);  
+	      }
+        }  		     
+      }  
+    } 
+  }
   return data.categories;  
 }
 
